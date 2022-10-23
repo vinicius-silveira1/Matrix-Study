43744aaaa84fcf35c91f7562f9224a693f461846
Vinícius Silveira <vinifs19@gmail.com>
2022-10-23 10:46:59 -0300

    Added main variables

--- Matrix/src/App.java
+++ Matrix/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        int vector[] = {2, 4, 6, 8, 10};
+        int matrix[][] = new int [2][3];
+        String msg = "vector = ";
     }
 }
