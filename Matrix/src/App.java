4f51bab731c0ee86f80407f5d824dd2914659e8c
Vinícius Silveira <vinifs19@gmail.com>
2022-10-23 10:53:19 -0300

    Added loop to fill msg with vector items

--- Matrix/src/App.java
+++ Matrix/src/App.java
@@ -1,7 +1,17 @@
+import javax.swing.JOptionPane;
+
+import java.swing.*;
+
 public class App {
     public static void main(String[] args)  {
         int vector[] = {2, 4, 6, 8, 10};
         int matrix[][] = new int [2][3];
         String msg = "vector = ";
+
+
+        for (int i = 0; i < vector.length; i++) {
+            msg = msg + vector[i] + "  ";
+        }
+        JOptionPane.showMessageDialog(null, msg);
     }
 }
