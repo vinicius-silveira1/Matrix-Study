826678d92d9cdbd84f4f0a55395dcccf8f3b5eb5
Vinícius Silveira <vinifs19@gmail.com>
2022-10-23 10:58:59 -0300

    Added logic to fill matrix with user input

--- Matrix/src/App.java
+++ Matrix/src/App.java
@@ -13,5 +13,14 @@ public class App {
             msg = msg + vector[i] + "  ";
         }
         JOptionPane.showMessageDialog(null, msg);
+
+        msg = "Matrix = \n\n";
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Type a number to position " + i + "and " + j));
+                msg = msg + matrix[i][j] + "  ";
+            }
+        }
     }
 }
